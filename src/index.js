f8650555fb303f7f60d3d9d24a4c280e7218d645
rabotnik oleg <snapplay2138@gmail.com>
2025-05-07 12:36:30 +0300

    Fix: correct routes import

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.json());
 
 // Подключаем все маршруты под префиксом /api
-app.use('/api', routes);
+app.use('/api', require('./routes'));
 
 app.use('/uploads', express.static('uploads'))
 
